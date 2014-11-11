d5778f5998676a2fa96815d82cf81f585cb128ff
Oliver Wickham <mr.o.wickham@gmail.com>
2014-11-11 12:00:04 +0000

    Add tests for the ItemUserAverageRecommender because the SlopeOneRecommender is obsolete. The error returned is within the same tolerance

--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.4"
 
-// Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test"
 
 libraryDependencies += "org.apache.mahout" % "mahout-core" % "0.9"
+
+scalacOptions in ThisBuild ++= Seq("-unchecked", "-deprecation")
--- src/test/scala/RecommenderSpec.scala
+++ src/test/scala/RecommenderSpec.scala
@@ -5,7 +5,7 @@ import org.apache.mahout.cf.taste.impl.eval.{GenericRecommenderIRStatsEvaluator,
 import org.apache.mahout.cf.taste.impl.model.GenericDataModel
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
+import org.apache.mahout.cf.taste.impl.recommender.{ItemUserAverageRecommender, GenericUserBasedRecommender}
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity
 import org.apache.mahout.cf.taste.model.{PreferenceArray, DataModel}
 import org.apache.mahout.common.RandomUtils
@@ -18,51 +18,67 @@ class RecommenderSpec extends WordSpec with Matchers {
   lazy val groupLensModel = new FileDataModel(new File("ua.base"))
   RandomUtils.useTestSeed()
 
-  "Recommender" should {
+  "User based recommender" should {
 
     "recommend item 104" in {
       val similarity = new PearsonCorrelationSimilarity(introModel)
       val neighbourhood = new NearestNUserNeighborhood(2, similarity, introModel)
       val recommender = new GenericUserBasedRecommender(introModel, neighbourhood, similarity)
       val recommendations = recommender.recommend(1, 1)
-      recommendations.head.getItemID should be === 104
-      recommendations.head.getValue should be === 4.257081f
+      recommendations.head.getItemID should be (104)
+      recommendations.head.getValue should be (4.257081f)
     }
 
     "have an error of 1.0 using AverageAbsoluteDifferenceRecommender" in {
       val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator
 
-      val score = evaluator.evaluate(recommenderBuilder, dataModelBuilder, introModel, 0.9, 1.0)
-      score should be === 1.0
+      val score = evaluator.evaluate(userbasedRecommenderBuilder, dataModelBuilder, introModel, 0.9, 1.0)
+      score should be (1.0)
     }
 
     "have precision of 0.75 and recall of 1.0 using RecommenderIRStatsEvaluator" in {
       val evaluator = new GenericRecommenderIRStatsEvaluator()
 
       val stats = evaluator.evaluate(
-         recommenderBuilder, dataModelBuilder, introModel, null, 2, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 1.0)
+         userbasedRecommenderBuilder, dataModelBuilder, introModel, null, 2, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 1.0)
 
-      stats.getPrecision should be === 0.75
-      stats.getRecall should be === 1.0
+      stats.getPrecision should be (0.75)
+      stats.getRecall should be (1.0)
     }
 
     "have error of 0.9 with Grouplens data" in {
       val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator
 
-      val score = evaluator.evaluate(recommenderBuilder, dataModelBuilder, groupLensModel, 0.9, 1.0)
+      val score = evaluator.evaluate(userbasedRecommenderBuilder, dataModelBuilder, groupLensModel, 0.9, 1.0)
       score should be (0.9 +- 0.05)
     }
 
-    def dataModelBuilder = new DataModelBuilder() {
-      def buildDataModel(trainingData: FastByIDMap[PreferenceArray]) = new GenericDataModel(trainingData)
-    }
-
-    def recommenderBuilder = new RecommenderBuilder() {
+    def userbasedRecommenderBuilder = new RecommenderBuilder() {
       def buildRecommender(dm: DataModel) = {
         val similarity = new PearsonCorrelationSimilarity(dm)
         val neighbourhood = new NearestNUserNeighborhood(2, similarity, dm)
         new GenericUserBasedRecommender(dm, neighbourhood, similarity)
       }
     }
+  }
+
+  // The SlopeOne recommender was deprecated in Mahoot 0.8
+  // https://issues.apache.org/jira/browse/MAHOUT-1250
+  "Item user average recommender" should {
+
+    "have an error of 0.748 with Grouplens data" in {
+      val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator
+
+      val score = evaluator.evaluate(itemUserAverageRecommenderBuilder, dataModelBuilder, groupLensModel, 0.9, 1.0)
+      score should be (0.78 +- 0.05)
+    }
+
+    def itemUserAverageRecommenderBuilder = new RecommenderBuilder() {
+      def buildRecommender(dm: DataModel) = new ItemUserAverageRecommender(dm)
+    }
+  }
+
+  def dataModelBuilder = new DataModelBuilder() {
+    def buildDataModel(trainingData: FastByIDMap[PreferenceArray]) = new GenericDataModel(trainingData)
   }
 }
